8ee51d8a534193badfb6dca83ab1f65086c71e54
akiladevinda <akiladevinda96@gmail.com>
2016-10-06 10:54:22 +0530

    loginConnect.php file updated

--- loginConnect.php
+++ loginConnect.php
@@ -22,7 +22,7 @@ $result = mysqli_query($conn,$sql);
 if(!$row = mysqli_fetch_assoc($result)){
 	echo "Usrename or pass incorrect ";
 }else {
- 	$_SESSION['ID'] = $row['ID'];
+ 	$_SESSION['username'] = $row['username'];
 
 }
 header('Location:loginForm.php');
