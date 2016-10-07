79e88ac248b9931a76e52e029faf181e030f74d4
44imtk44 <imtk4444@gmail.com>
2016-10-07 13:11:15 +0530

    management page was added...

---
+++ viewMgtNotes.php
@@ -0,0 +1,109 @@
+<?php 
+  session_start();
+ ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Computing</title>
+
+	<link href="style.css" rel="stylesheet" type="text/css">
+
+</head>
+<body background="mgt.jpg">
+
+
+<div id="header-wrap">  
+
+<ul>
+  <li><img src="eLeraningLogo.jpg" width="300px" height="45px" border="2"></li>
+  <li><a class="active-home" href="main.php">Home</a></li>
+  <li><a class="active-home" href="http://www.nsbm.lk/news">News</a></li>
+  <li class="dropdown">
+    <a href="#" class="dropbtn">Courses</a>
+    <div class="dropdown-content">
+      <a href="computing.php">Computing</a>
+      <a href="management.php">Business</a>
+      <a href="engineering.php">Engineering</a>
+    </div>
+</li>
+
+
+    <li class="dropdown">
+    <a href="#" class="dropbtn">More</a>
+    <div class="dropdown-content">
+      <a href="http://lms.nsbm.lk/mod/resource/view.php?id=5655&subdir=/Computing">Class Time Tables</a>
+      <a href="#">Societies</a>
+      <a href="#">Clubs</a>
+      <a href="#">Activities</a>
+    </div>
+
+  </li>
+
+    <div id="regButton">
+  <a href="logOut.php"><input type="button" class="Lgbutton" value="Logout" align="right" ></a>
+  </div>
+
+      <div id="regButton">
+
+
+            <div class="notify">
+           <?php 
+          if (isset($_SESSION['username'])){
+            echo '<h3 style="color:#0CYF730"> Your Logged Username :'.$_SESSION['username'].'</h3>';
+          }else {
+            echo '<h3 style="color:#0CF730">You are not logged in !</h3>';
+          }
+
+         ?>
+
+         </div>
+  
+
+</ul>
+
+
+
+<h1 style="color:#0771F0 ; font-size: 20pt; font-family:gotham; text-align:center" >Course : Business Management </h1>
+
+
+<nav>
+  <ul>
+    <li><a href="management.php"><input type="button" class="buttonCom" value="Add Notes" align="right"></a></li><br><br><br><br>
+    <a href="viewMgNotes.php" class="linkToview">View Notes</a>
+  </ul>
+</nav>
+
+
+<?php
+
+
+$sql = "SELECT * FROM notes";
+
+$link=mysqli_connect('localhost','root','','mngNotes');
+
+$data = mysqli_query($link,$sql);
+
+
+
+
+
+while ( $row = mysqli_fetch_array($data) )
+{
+
+   echo '<textarea name="txtArea"  rows="30" cols="30" style="background-color:#C4E0F3; color:black; ">'.$row[2].'</textarea>';
+
+}
+
+
+
+
+
+
+ ?>
+
+
+
+</body>
+</html>
